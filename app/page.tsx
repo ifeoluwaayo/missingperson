b7da4b86f68d7943305cb35593800cb2d70f157e
ifeoluwaayo <adeayomide112@gmail.com>
2023-02-02 17:57:00 +0100

    new

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Page() {
 	return (
-		<div className="flex w-full h-screen items-center justify-center">
+		<div className="flex w-full h-screen items-center justify-center flex-col">
 			<h1 className="text-4xl font-bold">Missing People | Nigeria.</h1>
 			<p className="text-xl font-semibold mt-2 text-red-500">
 				Coming Soon...
